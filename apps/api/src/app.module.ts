e72b664e711eef4b42009e59cb20fe8d43944166
bitkunst <bitkunst23@gmail.com>
2023-11-19 22:21:23 +0900

    feat: mysql-typeorm config library

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MySqlConfigService } from '@app/type-orm-config';
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            useClass: MySqlConfigService,
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+    constructor(private readonly configService: ConfigService) {}
+
     getHello(): string {
+        console.log(
+            "this.configService.get('DB_NAME')",
+            this.configService.get('DB_NAME'),
+        );
         return 'Hello World!!';
     }
 }
